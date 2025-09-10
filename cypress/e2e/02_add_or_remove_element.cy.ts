e348689ec645cee61088d8944b144ac5049308c9
madorjan2 <76928045+madorjan2@users.noreply.github.com>
2025-09-10 12:52:36 +0200

    prettier config

--- cypress/e2e/02_add_or_remove_element.cy.ts
+++ cypress/e2e/02_add_or_remove_element.cy.ts
@@ -1,31 +1,31 @@
-describe("Add or Remove Elements", () => {
-  beforeEach(() => {
-    cy.visit("/add_remove_elements/");
-  });
+describe('Add or Remove Elements', () => {
+	beforeEach(() => {
+		cy.visit('/add_remove_elements/');
+	});
 
-  const addElement = (): void => {
-    cy.contains("button", "Add Element").click();
-  };
+	const addElement = (): void => {
+		cy.contains('button', 'Add Element').click();
+	};
 
-  const deleteFirstElement = (): void => {
-    cy.contains("button", "Delete").first().click();
-  };
+	const deleteFirstElement = (): void => {
+		cy.contains('button', 'Delete').first().click();
+	};
 
-  it("should add one element per click", () => {
-    addElement();
-    cy.get("button").filter(':contains("Delete")').should("have.length", 1);
-    addElement();
-    addElement();
-    cy.get("button").filter(':contains("Delete")').should("have.length", 3);
-  });
+	it('should add one element per click', () => {
+		addElement();
+		cy.get('button').filter(':contains("Delete")').should('have.length', 1);
+		addElement();
+		addElement();
+		cy.get('button').filter(':contains("Delete")').should('have.length', 3);
+	});
 
-  it("should delete element on click", () => {
-    addElement();
-    addElement();
-    cy.get("button").filter(':contains("Delete")').should("have.length", 2);
-    deleteFirstElement();
-    cy.get("button").filter(':contains("Delete")').should("have.length", 1);
-    deleteFirstElement();
-    cy.get("button").filter(':contains("Delete")').should("have.length", 0);
-  });
+	it('should delete element on click', () => {
+		addElement();
+		addElement();
+		cy.get('button').filter(':contains("Delete")').should('have.length', 2);
+		deleteFirstElement();
+		cy.get('button').filter(':contains("Delete")').should('have.length', 1);
+		deleteFirstElement();
+		cy.get('button').filter(':contains("Delete")').should('have.length', 0);
+	});
 });
