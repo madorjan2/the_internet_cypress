47254d35e601d737dbc4241481e4b5022bb0b6ae
madorjan2 <76928045+madorjan2@users.noreply.github.com>
2025-09-12 13:03:15 +0200

    disabling experimental webkit support

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
 	e2e: {
+        experimentalWebKitSupport: false,
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
 		},
 		baseUrl: 'http://localhost:7080/',
 		viewportWidth: 1280,
 		viewportHeight: 720,
+        video: false,
+        screenshotOnRunFailure: false,
 	},
 });
