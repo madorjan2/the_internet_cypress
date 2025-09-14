4f0fec213d6e0f7138b763182c6cdbf04576efba
madorjan2 <76928045+madorjan2@users.noreply.github.com>
2025-09-14 09:21:43 +0200

    test 3 added cypress visit argument case

--- cypress/e2e/08_digest_auth.cy.ts
+++ cypress/e2e/08_digest_auth.cy.ts
@@ -1,26 +1,13 @@
 describe('Basic Auth', () => {
 	beforeEach(() => {});
 
-	it('should log in via basic auth after intercepting request', () => {
+	it('should login in via basic auth', () => {
 		cy.intercept('GET', '/basic_auth/', (req) => {
 			const credentials: string = btoa('admin:admin');
 			req.headers['Authorization'] = `Basic ${credentials}`;
 		}).as('authRequest');
 		cy.visit('/basic_auth/');
 		cy.wait('@authRequest');
-		cy.get('.example p').should(
-			'contain.text',
-			'Congratulations! You must have the proper credentials.',
-		);
-	});
-
-	it('should log in via basic auth using Cypress visit argument', () => {
-		cy.visit('/basic_auth/', {
-			auth: {
-				username: 'admin',
-				password: 'admin',
-			},
-		});
 		cy.get('.example p').should(
 			'contain.text',
 			'Congratulations! You must have the proper credentials.',
